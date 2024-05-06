ed2bddcbf5a92e954bb6f8c1dde967f117cc3c19
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2024-05-06 17:14:57 +0300

    Divided home component into separate reusable parts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from '@components/home/home.component';
 import { NavBarComponent } from '@components/nav-bar/nav-bar.component';
 import { SliderComponent } from '@components/home/slider/slider.component';
+import { SubscribeComponent } from './components/subscribe/subscribe.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CategoriesPostsComponent } from './components/categories-posts/categories-posts.component';
+import { TrendingComponent } from './components/trending/trending.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavBarComponent,
-    SliderComponent
+    SliderComponent,
+    SubscribeComponent,
+    FooterComponent,
+    CategoriesPostsComponent,
+    TrendingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/categories-posts/categories-posts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-posts',
+  templateUrl: './categories-posts.component.html',
+  styleUrl: './categories-posts.component.css'
+})
+export class CategoriesPostsComponent {
+
+}
