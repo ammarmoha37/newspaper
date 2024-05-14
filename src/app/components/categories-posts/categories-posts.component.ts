837b220cd75c2ba4037d26f60c9b8c242bc4cf81
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2024-05-14 15:11:26 +0300

    recent articles section connection with API

--- src/app/components/categories-posts/categories-posts.component.ts
+++ src/app/components/categories-posts/categories-posts.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Article } from 'src/app/models/articles.model';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-categories-posts',
   templateUrl: './categories-posts.component.html',
   styleUrl: './categories-posts.component.css'
 })
-export class CategoriesPostsComponent {
+export class CategoriesPostsComponent implements OnInit {
 
+  recentArticles: Article[] = [];
+
+  constructor(private articleService: ArticleService) {}
+
+  ngOnInit(): void {
+    this.articleService.getTopHeadlines().subscribe((data: any) => {
+      console.log(data);
+      this.recentArticles = data.articles;
+    });
+  }
 }
