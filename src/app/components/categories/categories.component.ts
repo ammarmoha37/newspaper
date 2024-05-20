916bb563fe55dbc5d2e927e3336c62d7ceffdd01
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2024-05-20 12:37:05 +0300

    categories page connection with the API

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Article } from 'src/app/models/articles.model';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
 
+  sportArticles: Article[];
+  politicsArticles: Article[];
+  scienceArticles: Article[];
+
+  constructor(private articleService: ArticleService) { }
+
+  ngOnInit(): void {
+    this.articleService.getCategories('sport').subscribe((data: any) => {
+      this.sportArticles = data.articles;
+    });
+
+    this.articleService.getCategories('politics').subscribe((data: any) => {
+      this.politicsArticles = data.articles;
+    });
+
+    this.articleService.getCategories('science').subscribe((data: any) => {
+      this.scienceArticles = data.articles;
+    });
+  }
 }
