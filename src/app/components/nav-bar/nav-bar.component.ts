4236c0a347526f93a99b7c079e74159bdf73f62a
Ammar <101853260+ammarmoha37@users.noreply.github.com>
2024-05-30 22:26:54 +0300

    applying responsive design

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,19 +1,39 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
   query: string = '';
+  showMenu: boolean = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private elementRef: ElementRef) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showMenu = false;
+      }
+    });
+    document.addEventListener('click', this.onClick.bind(this));
+  }
 
   search(): void {
     if (this.query.trim()) {
       this.router.navigate(['/search'], { queryParams: { q: this.query } });
     }
+  }
+
+  onClick(event: MouseEvent) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.showMenu = false;
+    }
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
   }
 }
